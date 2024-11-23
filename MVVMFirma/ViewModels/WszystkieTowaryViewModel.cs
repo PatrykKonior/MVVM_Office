074f71091b12b3457fb27be782a6a29d6842f63c
Patryk <patrykkonior4@gmail.com>
2024-11-23 19:55:24 +0100

    Wyswietlanie materiałów

--- MVVMFirma/ViewModels/WszystkieTowaryViewModel.cs
+++ MVVMFirma/ViewModels/WszystkieTowaryViewModel.cs
@@ -5,21 +5,66 @@ using System.Text;
 using System.Collections.ObjectModel;
 using System.Windows.Documents;
 using MVVMFirma.Models.Entities;
+using MVVMFirma.Helper;
+using System.Windows.Input;
 
 namespace MVVMFirma.ViewModels
 {
     public class WszystkieTowaryViewModel : WorkspaceViewModel
     {
+        #region DB
+        private readonly DesignOfficeEntities designOfficeEntities;
 
+        
+        #endregion
+        #region LoadCommand
+        private BaseCommand _LoadCommand;
+        #endregion
+        #region Properties
+        public ICommand LoadCommand
+        {
+            get
+            {
+                if (_LoadCommand == null)
+                {
+                    _LoadCommand = new BaseCommand(() => load());
+                }
+                return _LoadCommand;
+            }
+        }
+        #endregion
+        #region List
+        private ObservableCollection<Materials> _List;
+        public ObservableCollection<Materials> List
+        {
+            get
+            {
+                if (_List == null)
+                    load();
+                return _List;
+            }
+            set
+            {
+                _List = value;
+                OnPropertyChanged(() => List);
+            }
+        }
+        #endregion
         public WszystkieTowaryViewModel()
         {
             base.DisplayName = "Towary";
+            designOfficeEntities = new DesignOfficeEntities();
 
         }
-
-        private readonly DesignOfficeEntities designOfficeEntities;
         #region Helpers
-
+        private void load()
+        {
+            List = new ObservableCollection<Materials>
+            (
+                designOfficeEntities.Materials.ToList()
+            );
+        }
+        #endregion
     }
 
 
