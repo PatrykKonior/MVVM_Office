e920f5d45bb7561b61174a17f413a47d26a13415
Patryk <patrykkonior4@gmail.com>
2024-11-23 20:51:22 +0100

    Dodawanie Materiału

---
+++ MVVMFirma/ViewModels/JedenViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using MVVMFirma.Helper;
+using MVVMFirma.Models.Entities;
+
+
+namespace MVVMFirma.ViewModels
+{
+    public abstract class JedenViewModel<T> : WorkspaceViewModel
+    {
+        #region DB
+
+        protected DesignOfficeEntities designOfficeEntities;
+
+        #endregion
+
+        #region Item
+
+        protected T item;
+
+        #endregion
+        #region Command
+
+        // to jest koenda, która zostanie podpięta pod przycisk zapisz i zamknij i ona wywoła funkcje SaveAndClose
+        private BaseCommand _SaveCommand;
+
+        public ICommand SaveCommand
+
+        {
+            get
+            {
+                if (_SaveCommand == null)
+                    _SaveCommand = new BaseCommand(() => SaveAndClose());
+                return _SaveCommand;
+            }
+        }
+
+        #endregion
+        #region Constructor
+
+        public JedenViewModel(string displayName)
+        {
+            base.DisplayName = displayName;
+            designOfficeEntities = new DesignOfficeEntities();
+        }
+
+        #endregion
+
+        #region Helpers
+
+        public abstract void Save();
+        public void SaveAndClose()
+        {
+            Save();
+            base.OnRequestClose(); //zamknięcie zakładki
+        }
+
+        #endregion
+    }
+}
--- MVVMFirma/ViewModels/NowyTowarViewModel.cs
+++ MVVMFirma/ViewModels/NowyTowarViewModel.cs
@@ -2,14 +2,88 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Input;
+using MVVMFirma.Helper;
+using MVVMFirma.Models.Entities;
 
 namespace MVVMFirma.ViewModels
 {
-    public class NowyTowarViewModel : WorkspaceViewModel
+    public class NowyTowarViewModel : JedenViewModel<Materials>
     {
+        #region Constructor
+
         public NowyTowarViewModel()
+        : base("Towar")
         {
-            base.DisplayName = "Towar";
+            item = new Materials();
         }
+
+        #endregion
+
+        #region Properties
+
+        // dla każdego pola na interface tworzymy properties
+        public string MaterialName
+        {
+            get
+            {
+                return item.MaterialName;
+            }
+            set
+            {
+                item.MaterialName = value;
+                OnPropertyChanged(() => MaterialName);
+            }
+        }
+        public string MaterialDescription
+        {
+            get
+            {
+                return item.MaterialDescription;
+            }
+            set
+            {
+                item.MaterialDescription = value;
+                OnPropertyChanged(() => MaterialDescription);
+            }
+        }
+
+        public decimal? UnitPrice
+        {
+            get
+            {
+                return item.UnitPrice;
+            }
+            set
+            {
+                item.UnitPrice = value;
+                OnPropertyChanged(() => UnitPrice);
+            }
+        }
+
+        public decimal? VATRate
+        {
+            get
+            {
+                return item.VATRate;
+            }
+            set
+            {
+                item.VATRate = value;
+                OnPropertyChanged(() => VATRate);
+            }
+        }
+
+
+        #endregion
+
+        #region Helpers
+
+        public override void Save()
+        {
+            designOfficeEntities.Materials.Add(item); //dodaje towar do lokalnej kolekcji
+            designOfficeEntities.SaveChanges(); //zapisuje zmiany do bazy danych
+        }
+        #endregion
     }
 }
