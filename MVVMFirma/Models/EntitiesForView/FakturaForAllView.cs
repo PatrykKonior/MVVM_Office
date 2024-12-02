a935df53f3fec198af4e20441a9dee7e456f3588
Patryk <patrykkonior4@gmail.com>
2024-12-02 20:56:34 +0100

    LINQ do Invoices przy pomocy Entity Framework

---
+++ MVVMFirma/Models/EntitiesForView/FakturaForAllView.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMFirma.Models.EntitiesForView
+{
+    // ta klasa jest wzorowana na klasie Faktura, tylko zamiast kluczy obcych ma czytelne dla Klienta pola (klucze obce będą zastąpione czytelnymi danymi)
+    public class FakturaForAllView
+    {
+        public int InvoiceID { get; set; }
+
+        public DateTime? SalesSaleDate { get; set; }
+
+        public decimal? SalesTotalNetAmount { get; set; }
+
+        public decimal? SalesTotalVATAmount { get; set; }
+
+        public decimal? SalesTotalGrossAmount { get; set; }
+
+        public string SalesSaleStatus { get; set; }
+
+        public DateTime? InvoiceDate { get; set; }
+
+        public DateTime? PaymentDueDate { get; set; }
+
+        public string InvoiceStatus { get; set; }
+
+        public decimal? TotalAmount { get; set; }
+    }
+}
--- MVVMFirma/ViewModels/WszystkieFakturyViewModel.cs
+++ MVVMFirma/ViewModels/WszystkieFakturyViewModel.cs
@@ -6,14 +6,44 @@ using System.Collections.ObjectModel;
 using MVVMFirma.Models.Entities;
 using MVVMFirma.Helper;
 using System.Windows.Input;
+using System.Windows.Documents;
+using MVVMFirma.Models.EntitiesForView;
 
 namespace MVVMFirma.ViewModels
 {
-    public class WszystkieFakturyViewModel:WorkspaceViewModel
+    public class WszystkieFakturyViewModel:WszystkieViewModel<FakturaForAllView>
     {
+        #region Constructor
         public WszystkieFakturyViewModel()
+            : base("Faktury")
         {
-            base.DisplayName = "Wszystkie faktury";
         }
+        #endregion
+
+        #region Helpers
+        public override void Load()
+        {
+            List = new ObservableCollection<FakturaForAllView>
+            (
+                List = new ObservableCollection<FakturaForAllView>
+                (
+                    from faktura in designOfficeEntities.Invoices
+                    select new FakturaForAllView
+                    {
+                        InvoiceID = faktura.InvoiceID,
+                        SalesSaleDate = faktura.Sales.SaleDate,
+                        SalesTotalNetAmount = faktura.Sales.TotalNetAmount,
+                        SalesTotalVATAmount = faktura.Sales.TotalVATAmount,
+                        SalesTotalGrossAmount = faktura.Sales.TotalGrossAmount,
+                        SalesSaleStatus = faktura.Sales.SaleStatus,
+                        InvoiceDate = faktura.InvoiceDate,
+                        PaymentDueDate = faktura.PaymentDueDate,
+                        InvoiceStatus = faktura.InvoiceStatus,
+                        TotalAmount = faktura.TotalAmount
+                    }
+                    )
+            );
+        }
+        #endregion
     }
 }
